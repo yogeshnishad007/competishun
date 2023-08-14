5d8ce57e10c22a0d621fd25559bd27ae76a1c9dd
yogeshnishad007 <yogiatoy27518@gmail.com>
2023-08-14 15:16:42 +0530

    login modify

--- Task-1/src/Components/Signin.jsx
+++ Task-1/src/Components/Signin.jsx
@@ -1,68 +1,65 @@
 import { useState } from "react";
-import {auth,provider} from "../Auth/FirebaseConfig";
-import {signInWithPopup} from "firebase/auth";
-import AllRoutes from "./AllRoutes"
-import"../Style/SignIn.css"
+import { auth, provider } from "../Auth/FirebaseConfig";
+import { signInWithPopup } from "firebase/auth";
+import AllRoutes from "./AllRoutes";
+import "../Style/SignIn.css";
 
+const SignIn = () => {
+  const [value, setValue] = useState(null);
 
-const SignIn =()=> {
-    const [value,setValue] = useState(null)
+  const handleClick = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        setValue(user);
 
-    const handleClick=()=>{
-        signInWithPopup(auth, provider).then((result)=>{
-          const user = result.user;
-          console.log(user);
-          setValue(user);
-          
-          localStorage.setItem("email",user.email)
+        localStorage.setItem("email", user.email);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const handleLogout = () => {
+    localStorage.clear();
+    window.location.reload();
+    setValue(null);
+  };
 
-        }).catch((err)=>{
-          console.log(err);
-        })
-      }
-    const handleLogout =()=>{
-        localStorage.clear()
-        window.location.reload()
-        setValue(null)
-    }
+  console.log("value", value);
 
-    console.log("value",value)
-
-return (
+  return (
     <div>
-        {value?(
-            <>
-            <div className="profile-container">
-               
-            <div className='profile-pic'>
-                <img src={value.photoURL} alt="dp" referrerPolicy='no-referrer'/>
-              </div>
-
-              <h3>Welcome {value.displayName}</h3>
-
-              <button className=''
-                        onClick={handleLogout}>
-                        LOGOUT
-               </button>
+      {value ? (
+        <>
+          <div className="profile-container">
+            <div className="profile-pic">
+              <img src={value.photoURL} alt="dp" referrerPolicy="no-referrer" />
             </div>
 
-            <div>
+            <h3>Welcome {value.displayName}</h3>
+
+            <button className="" onClick={handleLogout}>
+              LOGOUT
+            </button>
+          </div>
+
+          <div>
             <AllRoutes displayName={value.displayName} />
-            </div>
+          </div>
+        </>
+      ) : (
+        <>
+             <div className="sign-btn">
 
-          
-             
-            </>
-          ):(
-
-            <button onClick={handleClick}>Signin With Google</button>
-          )
-        
-        }
+                     <h1>Welcome to MoviX</h1>
+                 <button onClick={handleClick}>Signin With Google</button>
+             </div>
+               
+        </>
+       
+      )}
     </div>
-);
-}
-export default SignIn
-
-
-
+  );
+};
+export default SignIn;
