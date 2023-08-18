dbfeae36db54cc4265fe7f0d3ba68c8f5feab910
yogeshnishad007 <yogiatoy27518@gmail.com>
2023-08-18 23:06:18 +0530

    new update

--- Task-1/src/Components/MovieDetails.jsx
+++ Task-1/src/Components/MovieDetails.jsx
@@ -1,10 +1,10 @@
-import {useEffect,useState} from 'react';
-import { useParams} from 'react-router-dom';
-import NavBar from './NavBar';
-import "../Style/MovieDetails.css"
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import NavBar from "./NavBar";
+import "../Style/MovieDetails.css";
 
-const apiKey = 'c5a2b9797c94ce965b31f43c8078586b';
-const tmdbBaseUrl = 'https://api.themoviedb.org/3';
+const apiKey = "c5a2b9797c94ce965b31f43c8078586b";
+const tmdbBaseUrl = "https://api.themoviedb.org/3";
 
 const MovieDetails = () => {
   const { id } = useParams();
@@ -16,45 +16,46 @@ const MovieDetails = () => {
 
   const fetchMovieDetails = async () => {
     try {
-      const response = await fetch(`${tmdbBaseUrl}/movie/${id}?api_key=${apiKey}`);
+      const response = await fetch(
+        `${tmdbBaseUrl}/movie/${id}?api_key=${apiKey}`
+      );
       const data = await response.json();
       setMovie(data);
-      console.log("data",data)
+      console.log("data", data);
     } catch (error) {
       console.error(error);
     }
   };
 
   const handleWatchlist = () => {
-   
-    console.log('Added to Watchlist:', movie.title);
+    console.log("Added to Watchlist:", movie.title);
   };
 
   if (!movie) {
     return <div>Loading...</div>;
   }
 
-  const genreNames = movie.genres.map((el) => el.name).join(', ');
+  const genreNames = movie.genres.map((el) => el.name).join(", ");
 
   return (
     <div>
+      <NavBar />
 
-        <NavBar/>
+      <div className="card-box">
+        <img
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          alt={movie.title}
+        />
+        <h2>{movie.title}</h2>
+        <p>{genreNames}</p>
 
-                     
-
-           <div className='card-box'>
-
-                   <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>
-                    <h2>{movie.title}</h2>
-                    <p>{genreNames}</p>
-
-                    <button onClick={handleWatchlist}>Add to Watchlist</button>
-                    
-           </div>
-         
+        <div>
+          <button onClick={handleWatchlist}>Add to Watchlist</button>
+          <button>Add to Favorites</button>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default MovieDetails;
